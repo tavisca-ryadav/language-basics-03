87559ca960444f6a62b52e970bfdb5d3c5577411
rajat yadav <51901565+tavisca-ryadav@users.noreply.github.com>
2019-06-24 20:13:46 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -44,12 +44,12 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             var length1 = protein.Length;
             var length2 = dietPlans.Length;
 
-            int[] array = new int[length2];
-            int[] calorie = new int[length1];
+            var array = new int[length2];
+            var calories = new int[length1];
 
             // Calculate calorie for each element
             for(var i=0;i<length1;i++){
-                calorie[i]= (protein[i]*5 + carbs[i]*5 + fat[i]*9);
+                calories[i]= (protein[i]*5 + carbs[i]*5 + fat[i]*9);
             }
          
 
@@ -57,14 +57,14 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 var stringLen = dietPlans[i].Length;
 
                 //Use a visit array which will keep track of duplicate element values
-                int[] visit  = new int[length1];
+                var visit  = new int[length1];
 
                 for(int j=0;j<length1;j++)
                     visit[j]=1;
 
                 for(int j=0;j<stringLen;j++){
                     
-                    //Update visit array according to duplicacy in nutrients
+                    //Update visit array according to duplicacy in nutrition
 
                     if(dietPlans[i][j]=='p'){
                         MinCheck(protein,visit);
@@ -85,10 +85,10 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                         MaxCheck(fat,visit);
                     }
                     if(dietPlans[i][j]=='t'){
-                        MinCheck(calorie,visit);
+                        MinCheck(calories,visit);
                     }
                     if(dietPlans[i][j]=='T'){
-                        MaxCheck(calorie,visit);
+                        MaxCheck(calories,visit);
                     }
 
                     //Store index in array 
@@ -101,43 +101,43 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             return array;
         }
 
-        public static void MinCheck(int[] nutrient,int[] visit){
-            var min = 2000;
+        private static void MinCheck(int[] nutrient,int[] visit){
+            var minNutrient = 2000;
             var length = nutrient.Length;
             //First find the minmum element in nutrient
             for(var k=0;k<length;k++){
-                if(nutrient[k]<min && visit[k]==1){
-                    min = nutrient[k];
+                if(nutrient[k]<minNutrient && visit[k]==1){
+                    minNutrient = nutrient[k];
                 }
             }
             //Now check the duplicates on nutrient and update visit array
             for(var k=0;k<length;k++){
-                if(nutrient[k]==min)
+                if(nutrient[k]==minNutrient)
                     visit[k]=1;
                 else
                     visit[k]=0;
             }
         }
 
-        public static void MaxCheck(int[] nutrient,int[] visit){
-            var max=0;
+        private static void MaxCheck(int[] nutrient,int[] visit){
+            var maxNutrient=0;
             var length=nutrient.Length;
             //First find the maximum element in nutrient
             for(var k=0;k<length;k++){
-                if(nutrient[k]>max && visit[k]==1){
-                    max = nutrient[k];
+                if(nutrient[k]>maxNutrient && visit[k]==1){
+                    maxNutrient = nutrient[k];
                 }
             }
              //Now check the duplicates on nutrient and update visit array
             for(var k=0;k<length;k++){
-                if(nutrient[k]==max)
+                if(nutrient[k]==maxNutrient)
                     visit[k]=1;
                 else
                     visit[k]=0;
             }
         }
 
-        public static int Check(int[] visit){
+        private static int Check(int[] visit){
             //Find for the first 1 in array visit and return the index else 0
             for(var i=0;i<visit.Length;i++){
                 if(visit[i]==1)
